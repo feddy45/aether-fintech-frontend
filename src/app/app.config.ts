9e54e3991395d2e5a669eb2d28c330d26027ccfd
Federico Ghezzo <federico.ghezzo@intre.it>
2025-03-16 14:49:41 +0100

    💄 Add some scss from template

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,10 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), providePrimeNG({
     theme: {
       preset: Aura,
+      options: {
+        prefix: 'aef',
+        
+      },
     },
   }), provideAnimationsAsync()],
 };
