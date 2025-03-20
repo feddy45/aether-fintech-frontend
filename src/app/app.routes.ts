dd7a004a6ad50c52be199e331467abe9bca2d215
Federico Ghezzo <federico.ghezzo@intre.it>
2025-03-20 11:34:28 +0100

    ✨ Add dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Dasboard',
+    path: '',
+    component: DashboardComponent,
+  },
+];
