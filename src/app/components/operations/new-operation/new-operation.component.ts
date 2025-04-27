c5a1025b67abe3b69f0258c80fcbdf39fbc6a62d
Federico Ghezzo <federico.ghezzo@intre.it>
2025-04-27 10:59:24 +0200

    ✨ Show internal transfer if the user has more than one bank accounts

--- src/app/components/operations/new-operation/new-operation.component.ts
+++ src/app/components/operations/new-operation/new-operation.component.ts
@@ -1,23 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { BankTransferComponent } from './bank-transfer/bank-transfer.component';
 import { InternalTransferComponent } from './internal-transfer/internal-transfer.component';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primeng/tabs';
+import { BankAccountService } from '../../../services/bank-account/bank-account.service';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'aef-new-operation',
   imports: [
-    BankTransferComponent,
-    InternalTransferComponent,
     Tab,
     TabList,
     TabPanel,
     TabPanels,
     Tabs,
+    NgComponentOutlet,
 
   ],
   templateUrl: './new-operation.component.html',
   styleUrl: './new-operation.component.css',
 })
 export class NewOperationComponent {
+  bankAccountService = inject(BankAccountService);
 
+  operationsAvailable = computed(() => [
+    {
+      name: 'Bonifico',
+      component: BankTransferComponent,
+      active: true,
+    },
+    {
+      name: 'Giroconto',
+      component: InternalTransferComponent,
+      active: this.bankAccountService.userBankAccounts().length > 1,
+    },
+  ]);
 }
